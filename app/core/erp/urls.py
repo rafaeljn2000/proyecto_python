7d0d39dc385f9e6bb6b45e271f5d6a6948359e5a
unknown <rafaeljn2000@gmail.com>
2023-07-27 23:00:19 -0600

    respaldo2

--- app/core/erp/urls.py
+++ app/core/erp/urls.py
@@ -1,14 +1,8 @@
 from django.urls import path
-from core.erp.views import miprimeravista,misegundavista    
-app_name='erp'
+from core.erp.views.category.views import *
+
+app_name = 'erp'
 
 urlpatterns = [
-    path('uno/',miprimeravista,name='vista1'),
-    path('dos/',misegundavista,name='vista2')
-    
-    
+    path('category/list/', CategoryListView.as_view(), name='category_list'),
 ]
-    
-    
-    
-
